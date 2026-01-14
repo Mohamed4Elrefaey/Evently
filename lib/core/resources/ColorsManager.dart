7c4da4e457ec8bb4819a86202e28d54700681c39
mohamed <mohammedhamedelrefaey@gmail.com>
2026-01-14 14:12:05 +0200

    + Initialize Evently App

---
+++ lib/core/resources/ColorsManager.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+abstract class ColorsManager {
+  // ------>  light mode colors
+  static const Color mainColorLight = Color(0xff0E3A99);
+  static const Color disableColorLight = Color(0xffB9B9B9);
+  static const Color mainTextLight = Color(0xff1C1C1C);
+  static const Color secTextLight = Color(0xff686868);
+  static const Color backgroundLight = Color(0xffF4F7FF);
+  static const Color inputsLight = Color(0xffFFFFFF);
+  static const Color strokeLight = Color(0xffF0F0F0);
+  static const Color red = Color(0xffFF3232);
+
+  // ------> Dark mode colors
+  static const Color mainColorDark = Color(0xff457AED);
+  static const Color disableColorDark = Color(0xffB9B9B9);
+  static const Color mainTextDark = Color(0xffFFFFFF);
+  static const Color secTextDark = Color(0xffD6D6D6);
+  static const Color backgroundDark = Color(0xff000F30);
+  static const Color inputsDark = Color(0xff001440);
+  static const Color strokeDark = Color(0xff002D8F);
+}
---
+++ lib/core/resources/StringsManager.dart
@@ -0,0 +1,40 @@
+abstract class StringsManager {
+  static const splashTitle = "Supervised by Mohamed Nabil";
+  static const startTitle = "Personalize Your Experience";
+  static const startContent =
+      "Choose your preferred theme and language to get started with a comfortable, tailored experience that suits your style";
+  static const onboardingTitle1 = "Find Events That Inspire You";
+  static const onboardingTitle2 = "Effortless Event Planning";
+  static const onboardingTitle3 = "Connect with Friends & Share Moments";
+  static const onboardingContent1 =
+      "Dive into a world of events crafted to fit your unique interests. Whether you're into live music, art workshops, professional networking, or simply discovering new experiences, we have something for everyone. Our curated recommendations will help you explore, connect, and make the most of every opportunity around you.";
+  static const onboardingContent2 =
+      "Take the hassle out of organizing events with our all-in-one planning tools. From setting up invites and managing RSVPs to scheduling reminders and coordinating details, we’ve got you covered. Plan with ease and focus on what matters – creating an unforgettable experience for you and your guests.";
+  static const onboardingContent3 =
+      "Make every event memorable by sharing the experience with others. Our platform lets you invite friends, keep everyone in the loop, and celebrate moments together. Capture and share the excitement with your network, so you can relive the highlights and cherish the memories.";
+  static const language = "Language";
+  static const english = "English";
+  static const arabic = "Arabic";
+  static const theme = "Theme";
+  static const startBtn = "Let’s start";
+  static const nextBtn = "Next";
+  static const getStartedBtn = "Get started";
+  static const loginTitle = "Login to your account";
+  static const enterYourEmail = "Enter your email";
+  static const enterYourPassword = "Enter your password";
+  static const forgetPasswordAsk = "Forget Password?";
+  static const loginBtn = "Login";
+  static const signupBtn = "Sign up";
+  static const donNotHaveAccount = "Don’t have an account ?";
+  static const signupText = "Signup";
+  static const loginText = "Login";
+  static const or = "Or";
+  static const loginWithGoogle = "Login with Google";
+  static const signupWithGoogle = "Sign up with Google";
+  static const signupTitle = "Create your account";
+  static const enterYourName = "Enter your name";
+  static const confirmPassword = "Confirm your password";
+  static const alreadyHaveAccount = "Already have an account? Login";
+  static const forgetPasswordTitle = "Forget Password";
+  static const resetPasswordBtn = "Reset password";
+}
